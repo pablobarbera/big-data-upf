422ef7ee14d6735735d6a3798df4cc69fad6beb6
Pablo Barberá <pablo.barbera@nyu.edu>
2017-06-26 07:52:36 +0200

    added slides day 1

--- packages.r
+++ packages.r
@@ -1,6 +1,6 @@
 doInstall <- TRUE  # Change to FALSE if you don't want packages installed.
 toInstall <- c("ROAuth", "streamR", "ggplot2", "stringr",
                "maps", "Rfacebook", "topicmodels", "devtools", "quanteda",
-               "igraph", "rvest", "httr", "yaml")
+               "igraph", "rvest", "httr", "yaml", "formatR")
 if (doInstall) install.packages(toInstall)
 if (doInstall) devtools::install_github("netdem-usc/netdemR")
